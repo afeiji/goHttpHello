1a6ec2b341dc0855c9aaa90d8de3da487359d5c3
Your Name <you@example.com>
2022-02-14 11:17:23 +0800

    支持配置文件参数

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 ### golang简单打包测试项目，支持配置文件config.yaml
 ### 手动调试
-go mod init helloserver\n
-go mod tidy -compat=1.17    #自动安装依赖包\n
-go run helloserver.go\n
+go mod init helloserver  
+go mod tidy -compat=1.17    #自动安装依赖包  
+go run helloserver.go  
+
+编译打包测试版本修改下面内容  
+str := "Hello world ! friend01  
--- helloserver.go
+++ helloserver.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"io"
 	"log"
 	"net/http"
+	"os"
+	"path"
 	"strconv"
 )
 
@@ -19,15 +22,18 @@ type YamlConfig struct {
 
 var iCnt int = 0
 var yamlConfig YamlConfig
+var configFIle string
 
 func GetConfig() {
 	viperConfig := viper.New()
+	// 设置配置文件目录(可以设置多个,优先级根据添加顺序来)
+	dir, file := path.Split(configFIle)
+	viperConfig.AddConfigPath(dir)
 	// 设置配置文件名，没有后缀
-	viperConfig.SetConfigName("config")
+	viperConfig.SetConfigName(file)
 	// 设置读取文件格式为: yaml
 	viperConfig.SetConfigType("yaml")
-	// 设置配置文件目录(可以设置多个,优先级根据添加顺序来)
-	viperConfig.AddConfigPath(".")
+
 	// 读取解析
 	if err := viperConfig.ReadInConfig(); err != nil {
 		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
@@ -49,21 +55,37 @@ func GetConfig() {
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	iCnt++
-	str := "Hello world ! friend(" + strconv.Itoa(iCnt) + ")"
+        // 编译打包测试版本修改下面内容
+	str := "Hello world ! friend01(" + strconv.Itoa(iCnt) + ")"
 	io.WriteString(w, str)
 	fmt.Println(str)
 }
 
-func main() {
-	GetConfig()
-	fmt.Printf("YamlConfig: %#v", yamlConfig)
-	ht := http.HandlerFunc(helloHandler)
-	if ht != nil {
-		http.Handle("/hello", ht)
-	}
-	err := http.ListenAndServe(":"+yamlConfig.HTTP.Port, nil)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err.Error())
-	}
+func init() {
+	rootCmd.Flags().StringVarP(&configFIle, "config", "c", "./config.yml", "set config file")
 }
 
+var rootCmd = &cobra.Command{
+	Use:     `helloserver`,
+	Short:   `helloserver`,
+	Example: `helloserver`,
+	Run: func(cmd *cobra.Command, args []string) {
+		GetConfig()
+		fmt.Printf("YamlConfig: %#v", yamlConfig)
+		ht := http.HandlerFunc(helloHandler)
+		if ht != nil {
+			http.Handle("/hello", ht)
+		}
+		err := http.ListenAndServe(":"+yamlConfig.HTTP.Port, nil)
+		if err != nil {
+			log.Fatal("ListenAndServe: ", err.Error())
+		}
+	},
+}
+
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
